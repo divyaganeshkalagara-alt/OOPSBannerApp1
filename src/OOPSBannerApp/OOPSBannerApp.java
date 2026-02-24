b5315073ead16f115b3ea51e3ca2c3380243647c
divyaganeshkalagara-alt <divyaganeshkalagara@gmail.com>
2026-02-24 09:53:19 +0530

    UC2

--- src/OOPSBannerApp/OOPSBannerApp.java
+++ src/OOPSBannerApp/OOPSBannerApp.java
@@ -1,9 +1,11 @@
-public class OOPSBannerApp {
-
+class OopsWord {
     public static void main(String[] args) {
-
-        System.out.println( "00PS");
-
+        System.out.println("  *  "+"  *  "+"**"+" ** ");
+        System.out.println(" *   * "+" *   * "+"*    *"+" *      ");
+        System.out.println(" *   * "+" *   * "+"*    *"+" *      ");
+        System.out.println(" *   * "+" *   * "+"**"+" ** ");
+        System.out.println(" *   * "+" *   * "+"*     "+"      * ");
+        System.out.println(" *   * "+" *   * "+"*     "+"      * ");
+        System.out.println("  *  "+"  *  "+"*     "+" ** ");
     }
-
 }
